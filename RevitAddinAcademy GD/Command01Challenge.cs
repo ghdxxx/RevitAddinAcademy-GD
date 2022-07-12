c523d5d753ea07342535cedc28f994ba59b0ab5c
ghdxxx <desaihgayatri@gmail.com>
2022-07-11 23:51:32 -0400

    Changes

--- RevitAddinAcademy GD/Command01Challenge.cs
+++ RevitAddinAcademy GD/Command01Challenge.cs
@@ -14,7 +14,7 @@ using System.IO;
 namespace RevitAddinAcademy_GD
 {
     [Transaction(TransactionMode.Manual)]
-    public class Command01: IExternalCommand
+    public class Command01Challenge: IExternalCommand
     {
         public Result Execute(
           ExternalCommandData commandData,
@@ -27,29 +27,58 @@ namespace RevitAddinAcademy_GD
             Document doc = uidoc.Document;
 
            
-          
+        
+           
 
             double offset = 0.05;
-
             double offsetcalc = offset * doc.ActiveView.Scale;
-
-
             XYZ currentpoint = new XYZ(0, 0, 0);
-            XYZ offsetpoint = new XYZ(0, offsetcalc, 0); 
+            XYZ offsetpoint = new XYZ(0, offsetcalc, 0);
+            string text = "";
+
+
 
           
             FilteredElementCollector collector = new FilteredElementCollector(doc);
             collector.OfClass(typeof(TextNoteType));
 
-            Transaction t = new Transaction(doc, "create text note");
+            Transaction t = new Transaction(doc, "create FizzBuzz Text");
             t.Start();
 
             int range = 100;
 
             for (int i = 1; i <= range; i++)
+
+
             {
-                TextNote Enneadnote = TextNote.Create(doc, doc.ActiveView.Id, currentpoint, "This is Line" + i.ToString(), collector.FirstElementId());
-                currentpoint = currentpoint.Subtract(offsetpoint); 
+                if (i % 3 == 0 && i % 5 == 0)
+
+                { 
+                    text = "FizzBuzz";
+                }
+           
+                else if (i % 5 == 0)
+                {
+                    text = "Buzz";
+                }
+
+                else if (i % 3 == 0)
+                {
+                    text = "Fizz";
+                }
+
+
+          
+                 else
+                {
+                    text = i.ToString();
+
+                }
+                        
+
+
+                TextNote textNote = TextNote.Create(doc, doc.ActiveView.Id, currentpoint, text, collector.FirstElementId());
+                currentpoint = currentpoint. Subtract (offsetpoint); 
 
             }
 
